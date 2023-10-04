08b10ef6dfc21651ab64a5e70631ca747cd14324
Admin <theanh2002vcbn@gmail.com>
2023-10-04 11:41:00 +0700

    Generator

--- app/src/main/java/com/example/gamedomin/util/GameEngine.java
+++ app/src/main/java/com/example/gamedomin/util/GameEngine.java
@@ -1,6 +1,23 @@
 package com.example.gamedomin.util;
-
+import  android.content.Context;
 public class GameEngine {
+    private static GameEngine instance;
+    private static final int BOMB_NUMBER = 10;
+    private static final int WIDTH = 10;
+    private static final int HEIGHT = 10;
 
+    private  Context context;
 
+    public static GameEngine getInstance(){
+        if(instance == null){
+            instance = new GameEngine();
+        }
+        return instance;
+    }
+    private GameEngine(){}
+    public  void createGrid(Context context){
+        this.context = context;
+
+        int[][] GenerateGrid = Generator.generate(BOMB_NUMBER,WIDTH,HEIGHT);
+    }
 }
