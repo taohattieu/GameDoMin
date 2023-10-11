041d86f8f97095d158b3746eaf9c13638b2f1a58
Admin <vantaodz1@gmail.com>
2023-10-11 13:33:49 +0700

    image

--- app/src/main/java/com/example/gamedomin/trangchu.java
+++ app/src/main/java/com/example/gamedomin/trangchu.java
@@ -1,14 +1,13 @@
 package com.example.gamedomin;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class trangchu extends AppCompatActivity {
+public class trangchu extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
